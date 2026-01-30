1f3cc580e61e3e41071b179ae6c60398e8e2ce5f
Danu Renmi Wijaya <dremiceki@gmail.com>
2026-01-31 00:29:40 +0700

    fix: fixed error code at src/app/cart/page.tsx

--- milestone-3-danurenmiwijaya/src/app/cart/page.tsx
+++ milestone-3-danurenmiwijaya/src/app/cart/page.tsx
@@ -89,7 +89,7 @@ const CartPage = () => {
             <h1 className="text-2xl font-medium ">Your Shopping Cart</h1>
             <div className='flex flex-col lg:flex-row items-center gap-8 lg:gap-16'>
                 {steps.map(step=>(
-                    <div className={`flex items-center gap-2 border-b-2 pb-4 ${step.id===activeStep ? "border-gray-800": "border-gray-200"}`} key={step.id}>
+                    <div className={`flex items-center gap-2 border-b-2 pb-4 $ {step.id===activeStep ? "border-gray-800": "border-gray-200"}`} key={step.id}>
                         <div className={`w-6 h-6 rounded-full text-white p-4 flex items-center justify-center ${step.id===activeStep ? "bg-gray-800": "bg-gray-400"}`}>
                             {step.id}
                         </div>
